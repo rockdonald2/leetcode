a867bb104f2e4912e45aa497bedbb9d0ed5bf8fd
rockdonald2 <rockdonald2@gmail.com>
2020-02-18 14:59:13 +0200

    2020.02.18

---
+++ 3sum/3sum/3sum.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+	// létrehozunk egy vektort, ami tartalmazza a végső tripleteket
+	vector<vector<int>> result;
+
+	// abban az esetben ha a parametér vektor kevesebb taggal rendelkezik, mint 3, akkor üres vektort térítünk
+	if (nums.size() < 3) return vector<vector<int>>();
+
+	// a kulcs, hogy rendezzük az vektort növekvő sorrendben
+	sort(nums.begin(), nums.end());
+
+	/*
+	alapvetően az ötlet az, hogy kiválasztjuk az i. elemet, majd inicializáljuk a j-t és a k-t, amely a i utánadik, és az utolsó elemre mutat
+	a j. és k. összege, pont a 0 - i. elem kell legyen, abban az esetben, ha a kettő összege kisebb, akkor az j. elemről lépünk tovább,
+	ha nagyobb akkor a k-t csökkentsük
+	*/
+	for (int i = 0; i < nums.size() - 2; ++i) {
+		if (i > 0 && nums[i] == nums[i - 1]) continue; // ha az előző i. elem, megegyezik a mostani ugorjuk át, mert már számításba vettük
+
+		int wantedSum = 0 - nums[i];
+		int j = i + 1;
+		int k = nums.size() - 1;
+		while (j < k) {
+			int sum = nums[j] + nums[k];
+
+			if (sum == wantedSum) {
+				vector<int> triplet{ nums[i], nums[j], nums[k] };
+				result.push_back(triplet);
+
+				while (j < k && nums[j] == nums[j + 1]) ++j; // ha a következő elem megegyezik, ugorjuk át, már számításba vettük
+				while (j < k && nums[k] == nums[k - 1]) --k;
+
+				++j;
+				--k;
+			}
+			else if (sum < wantedSum) {
+				++j;
+			}
+			else {
+				--k;
+			}
+		}
+	}
+
+	return result;
+}
+
+int main() {
+	vector<int> nums{ -1, 0, 1, 2, -1, 4 };
+
+	vector<vector<int>> results = threeSum(nums);
+
+	for (int i = 0; i < results.size(); ++i) {
+		for (int j = 0; j < results[i].size(); ++j) {
+			cout << results[i][j] << " ";
+		}
+	}
+
+	return 0;
+}
---
+++ longestCommonPrefix/longestCommonPrefix/longestCommonPrefix.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+constexpr int CHAR_SIZE{ 128 };
+
+class Trie {
+public:
+	// létrehozunk két darab tagváltozót, egy bool-t, amely jelzi, hogy véget ért-e a szó behelyezéskor, kereséskor lesz hasznos, a másik pedig a Node-k tárolására van
+	bool isEndOfWord;
+	Trie* character[CHAR_SIZE]; // azért van a CHAR_SIZE konstanst, mert így fogjuk map-ként indexelni a betüket
+
+	// konstruktőr, amely inicializálja a tagváltozókat, a bool-t false-ra, a Node-kat pedig nullptr-re
+	Trie() {
+		this->isEndOfWord = false;
+
+		for (int i = 0; i < CHAR_SIZE; ++i) {
+			character[i] = nullptr;
+		}
+	}
+
+	void insert(string key) {
+		if (key == "") {
+			return;
+		}
+
+		// kezdjük a root-tól
+		Trie* curr = this;
+
+		// végig iterálunk az adott string-en
+		for (int i = 0; i < key.length(); ++i) {
+			// abban az esetben, ha az adott node nem létezik, akkor hozzáadjuk, és hozzárendelünk egy üres node-t.
+			if (curr->character[key[i]] == nullptr) {
+				curr->character[key[i]] = new Trie();
+			}
+
+			// átlépünk a következő node-ra.
+			curr = curr->character[key[i]];
+		}
+
+		// jelöljük a szó végét
+		curr->isEndOfWord = true;
+	}
+
+	string getLCP() {
+		// abban az esetben, ha a Trie nem létezne.
+		if (this == nullptr) {
+			return "";
+		}
+
+		// létrehozzuk a végső prefixet tartalmazó stringet
+		string lcp{ "" };
+
+		// elkezdjük végigjárni az összes node-t
+		Trie* curr = this;
+		for (int i = 0, flag = false; i < CHAR_SIZE; ++i) {
+			// ha az adott node nem üres
+			if (curr->character[i] != nullptr) {
+				// azért van szükség az if-else-re, mert ha két prefixet adnánk hozzá egy node-ról
+				if (!flag) {
+					lcp.push_back(static_cast<char>(i));
+					flag = true;
+				}
+				else {
+					lcp.pop_back();
+					break;
+				}
+			}
+
+			// ha az i=127
+			if (i == CHAR_SIZE - 1) {
+				// ha az lcp üres, tehát végig üres volt minden node, akkor lépjünk ki a loopból
+				if (lcp == "") break;
+
+				// átlépünk a következő char node-jára
+				curr = curr->character[lcp.back()];
+
+				// ha az adott node egy szó vége, akkor lépjünk ki,
+				// tudjuk, hogy nincs értelme tovább nézni, hiszen az egyik szó végéhez értünk
+				if (curr->isEndOfWord) {
+					break;
+				}
+
+				// reseteljük az iterátort és a flag-t, amely számon tartja, hogy 1 vagy több node-dal találkoztunk
+				i = 0;
+				flag = false;
+			}
+		}
+
+		return lcp;
+	}
+};
+
+string longestCommonPrefix(vector<string>& strs) {
+	if (strs.empty()) {
+		return "";
+	}
+
+	Trie t;
+	
+	for (int i = 0; i < strs.size(); ++i) {
+		// ha üres lenne bármelyik string, akkor üres lcp-t térítünk
+		if (strs[i] == "") {
+			return "";
+		}
+
+		t.insert(strs[i]);
+	}
+
+	return t.getLCP();
+}
+
+int main() {
+	vector<string> strs{ "", "b" };
+
+	cout << longestCommonPrefix(strs) << endl;
+
+	return 0;
+}
