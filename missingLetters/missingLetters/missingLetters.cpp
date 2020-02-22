859ce20a2b31b36c1ce5bce867f6758c70738c06
rockdonald2 <rockdonald2@gmail.com>
2020-02-22 22:00:19 +0200

    2020.02.22

---
+++ missingLetters/missingLetters/missingLetters.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// téríti az első betüt, amely hiányzik a stringből
+char missing(string& str) {
+	/*vector<char> ascii(256);
+
+	for (int i = 0; i < str.length(); ++i) {
+		ascii[str[i]] = 1;
+	}
+
+	for (int j = str[0]; j < ascii.size(); ++j) {
+		if (ascii[j] == 0) return j;
+	}
+
+	return NULL;*/
+
+	for (int i = 0; i < str.length(); ++i) {
+		if (i < str.length() - 1 && str[i] + 1 != str[i + 1]) return str[i] + 1;
+	}
+
+	return NULL;
+}
+
+int main() {
+	string str{ "stvwx" };
+
+	cout << missing(str) << endl;
+
+	return 0;
+}
