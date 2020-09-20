33d898ba0faa54195ec0f8e4e868f6e97519a0d9
Lukács Zsolt <rockdonald2@gmail.com>
2020-09-20 20:23:36 +0300

    2020.09.20

--- insertionSort/index.py
+++ insertionSort/index.py
@@ -2,7 +2,17 @@ from typing import List
 from random import randint
 
 def insertionSort(arr: List[int]) -> List[int]:
-    pass
+    for i in range(1, len(arr)):
+        curr = arr[i]
+        startIndex = i - 1
+
+        while startIndex >= 0 and arr[startIndex] > curr:
+            arr[startIndex + 1] = arr[startIndex]
+            startIndex -= 1
+
+        arr[startIndex + 1] = curr
+
+    return arr
 
 
 arr = [randint(0, 100) for _ in range(15)]
--- mergeSort/index.py
+++ mergeSort/index.py
@@ -1,11 +1,26 @@
 from typing import List
 from random import randint
 
-def merge():
-    pass
+def merge(leftArr, rightArr):
+    result = []
 
-def mergeSort():
-    pass
+    while len(leftArr) and len(rightArr):
+        result.append(leftArr.pop(0) if leftArr[0] < rightArr[0] else rightArr.pop(0))
+
+    result.extend(leftArr)
+    result.extend(rightArr)
+
+    return result
+
+def mergeSort(arr):
+    if len(arr) == 1:
+        return arr
+
+    midPoint = len(arr) // 2
+    leftArr = arr[:midPoint]
+    rightArr = arr[midPoint:]
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr))
 
 
 arr = [randint(0, 100) for _ in range(15)]
